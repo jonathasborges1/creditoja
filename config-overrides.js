2d06c1109ad348fb2b6d238d1aa6a6f7744d40fc
jonathasborges1 <jonathasborges1@users.noreply.github.com>
2023-04-03 01:44:17 +0000

    Deploy to GitHub Pages jonathasborges1/creditoja@15dfa4dc3d76afff4692bbccc5d820a9a68e862f

---
+++ config-overrides.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const Dotenv = require('dotenv-webpack');
+
+module.exports = function override(config,env) {
+  config.plugins.push(new Dotenv({ silent: true }));
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      ...config.alias,
+      "@assets": path.resolve(__dirname, "src/assets/"),
+      "@modules": path.resolve(__dirname, "src/modules/"),
+      "@ioc": path.resolve(__dirname, "src/ioc/"),
+      "@context": path.resolve(__dirname, "src/context/"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@config": path.resolve(__dirname, "src/config"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+    },
+  };
+
+  return config;
+};
